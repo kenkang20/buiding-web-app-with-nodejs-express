efc37f6639e5f6481ba0822f99b8c9ff0f60fdb3
ken <kkang@presagia.com>
2018-10-04 01:54:33 -0400

    source save

---
+++ library/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": [
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "airbnb-base"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "ecmaFeatures": {
+            "impliedStrict": true
+        },
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- library/app.js
+++ library/app.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var chalk = require('chalk');
+const express = require('express');
+const chalk = require('chalk');
 // DEBUG=* node app.js
 // Window: set DEBUG=* & node app.js, set DEBUG=app & node app.js
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
-//morgan('combined')
+// morgan('combined')
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -16,11 +17,11 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist/umd')));
 
-app.get('/', function (req, res) {
-    //res.send('Hello from my library app');
+app.get('/', (req, res) => {
+    // res.send('Hello from my library app');
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, function () {
-    debug(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+    debug(`listening at port ${chalk.green(port)}`);
 });
