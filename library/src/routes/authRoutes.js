134f3fd1a55f35c3a14b07ead202cd472039a711
ken <kkang@presagia.com>
2018-10-17 23:21:59 -0400

    source backup

---
+++ library/src/routes/authRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { MongoClient, ObjectID } = require('mongodb');
+const debug = require('debug')('app:authRoutes');
+
+const authRouter = express.Router();
+
+function router() {
+    authRouter.route('/signUp')
+        .post((req, res) => {
+            debug(req.body);
+            // create user
+            req.login(req.body, () => {
+                res.redirect('/auth/profile');
+            });
+        });
+
+    authRouter.route('/profile')
+        .get((req, res) => {
+            res.json(req.user);
+        });
+    return authRouter;
+}
+
+module.exports = router;
